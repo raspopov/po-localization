1303793fc104a6df77499dfa5062151245ddb05b
ryo.rabbit@gmail.com <ryo.rabbit@gmail.com@7e6a20d8-51de-4e2b-b00c-f9bd57077ef0>
2011-11-01 18:12:14 +0000

    Excluded .aps-files from pack.

--- trunk/pack.cmd
+++ trunk/pack.cmd
@@ -2,4 +2,4 @@ set version=1.0.0.0
 md "..\redist"
 del "..\redist\po-localization-%version%.zip"
 "%ProgramFiles%\WinRar\winrar.exe" a -ep -m5 -cfg- -afzip "..\redist\po-localization-%version%.zip" gpl-2.0.txt lgpl-2.1.txt Localization.h Localization.cpp ReadMe.txt Win32\release\exe2po.exe
-"%ProgramFiles%\WinRar\winrar.exe" a -m5 -cfg- -afzip "..\redist\po-localization-%version%.zip" sample\ sample\res -x*.user
+"%ProgramFiles%\WinRar\winrar.exe" a -m5 -cfg- -afzip "..\redist\po-localization-%version%.zip" sample\ sample\res -x*.user -x*.aps
