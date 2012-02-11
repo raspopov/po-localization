6d41686ca729a4dd4ea90fc9196acddef6b6edda
Nikolay Raspopov <raspopov@cherubicsoft.com>
2012-02-11 06:39:58 +0000

    Version bumped.

--- pack.cmd
+++ pack.cmd
@@ -1,4 +1,4 @@
-set version=1.0.0.0
+set version=1.0.0.1
 md "..\redist"
 del "..\redist\po-localization-%version%.zip"
 "%ProgramFiles%\WinRar\winrar.exe" a -ep -m5 -cfg- -afzip "..\redist\po-localization-%version%.zip" gpl-2.0.txt lgpl-2.1.txt Localization.h Localization.cpp ReadMe.txt COMPRESS.EXE Win32\release\exe2po.exe
