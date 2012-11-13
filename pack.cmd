1a81266b53288568475410be2fe32e246016cdb4
Nikolay Raspopov <raspopov@cherubicsoft.com>
2012-11-13 16:46:56 +0000

    Rearranged project files.

--- pack.cmd
+++ pack.cmd
@@ -1,5 +1,5 @@
 set version=1.0.0.2
 md "..\redist"
 del "..\redist\po-localization-%version%.zip"
-"%ProgramFiles%\WinRar\winrar.exe" a -ep -m5 -cfg- -afzip "..\redist\po-localization-%version%.zip" gpl-2.0.txt lgpl-2.1.txt Localization.h Localization.cpp ReadMe.txt COMPRESS.EXE Win32\release\exe2po.exe *.reg
+"%ProgramFiles%\WinRar\winrar.exe" a -ep -m5 -cfg- -afzip "..\redist\po-localization-%version%.zip" exe2po\gpl-2.0.txt src\lgpl-2.1.txt src\Localization.h src\Localization.cpp ReadMe.txt src\COMPRESS.EXE Win32\release\exe2po.exe *.reg
 "%ProgramFiles%\WinRar\winrar.exe" a -m5 -cfg- -afzip "..\redist\po-localization-%version%.zip" sample\ sample\res -x*.user -x*.aps
