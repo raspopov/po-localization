d7dea068f75e439a758cd727f9fbc116b4d1360f
ryo.rabbit@gmail.com <ryo.rabbit@gmail.com@7e6a20d8-51de-4e2b-b00c-f9bd57077ef0>
2012-11-14 16:09:25 +0000

    Version 1.0.0.3 bumped.

--- trunk/pack.cmd
+++ trunk/pack.cmd
@@ -1,4 +1,4 @@
-set version=1.0.0.2
+set version=1.0.0.3
 md "..\redist"
 del "..\redist\po-localization-%version%.zip"
 "%ProgramFiles%\WinRar\winrar.exe" a -ep -m5 -cfg- -afzip "..\redist\po-localization-%version%.zip" exe2po\gpl-2.0.txt src\lgpl-2.1.txt src\Localization.h src\Localization.cpp ReadMe.txt src\COMPRESS.EXE Win32\release\exe2po.exe *.reg
