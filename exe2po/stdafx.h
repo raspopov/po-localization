c4b16faac93dc25fe77c636d2d04930a5516b443
Nikolay Raspopov <raspopov@cherubicsoft.com>
2015-09-28 15:27:07 +0000

    Updated project up to VS2015, added 64-bit compilation.

--- exe2po/stdafx.h
+++ exe2po/stdafx.h
@@ -1,58 +1,54 @@
-//
-// stdafx.h
-//
-// EXE to PO file converter.
-// Copyright (c) Nikolay Raspopov, 2011-2015.
-// Web: https://sourceforge.net/projects/po-localization/
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
+/*
+This file is part of EXE to PO file converter (EXE2PO)
+https://sourceforge.net/projects/po-localization/
+
+Copyright (C) 2011-2015 Nikolay Raspopov <raspopov@cherubicsoft.com>
+
+This program is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+( at your option ) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see < http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
-
-#ifndef _SECURE_ATL
-#define _SECURE_ATL 1
-#endif
 
 #ifndef VC_EXTRALEAN
 #define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
 #endif
 
-#ifndef WINVER					// Specifies that the minimum required platform is Windows Vista.
-#define WINVER 0x0600			// Change this to the appropriate value to target other versions of Windows.
+#ifndef _SECURE_ATL
+#define _SECURE_ATL 1
 #endif
 
-#ifndef _WIN32_WINNT			// Specifies that the minimum required platform is Windows Vista.
-#define _WIN32_WINNT 0x0600		// Change this to the appropriate value to target other versions of Windows.
-#endif
-
-#ifndef _WIN32_WINDOWS          // Specifies that the minimum required platform is Windows 98.
-#define _WIN32_WINDOWS 0x0410	// Change this to the appropriate value to target Windows Me or later.
-#endif
-
-#ifndef _WIN32_IE				// Specifies that the minimum required platform is Internet Explorer 7.0.
-#define _WIN32_IE 0x0700		// Change this to the appropriate value to target other versions of IE.
-#endif
-
-#define WIN32_LEAN_AND_MEAN
-#define STRICT
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS
 #define _ATL_CSTRING_NO_CRT
 #define _ATL_NO_COM_SUPPORT
+#define _AFX_ALL_WARNINGS
+#define NO_PRINT
+
+#define _WIN32_WINNT 0x0501
+#include <sdkddkver.h>
 
 #include <atlbase.h>
 #include <atlwin.h>
 #include <atlstr.h>
+#include <atlcoll.h>
 #include <shlwapi.h>
+
+#ifdef _UNICODE
+#if defined _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+#endif
--- sample/stdafx.h
+++ sample/stdafx.h
@@ -1,52 +1,52 @@
-//
-// stdafx.h
-//
-// This file is part of Localization library.
-// Copyright (c) Nikolay Raspopov, 2011-2015.
-// Web: https://sourceforge.net/projects/po-localization/
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
+/*
+This file is part of Localization library
+https://sourceforge.net/projects/po-localization/
+
+Copyright (C) 2011-2015 Nikolay Raspopov <raspopov@cherubicsoft.com>
+
+This program is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+( at your option ) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see < http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
-#define _SECURE_ATL 1
+#ifndef VC_EXTRALEAN
 #define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
-#define WIN32_LEAN_AND_MEAN
-#define STRICT
+#endif
+
+#ifndef _SECURE_ATL
+#define _SECURE_ATL 1
+#endif
+
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS
 #define _ATL_CSTRING_NO_CRT
 #define _ATL_NO_COM_SUPPORT
 #define _AFX_ALL_WARNINGS
+#define NO_PRINT
 
-#include <SDKDDKVer.h>
-
-#include "miniVS2010.h"
+#define _WIN32_WINNT 0x0501
+#include <sdkddkver.h>
 
 #include <afxwin.h>
+
+#include "Localization.h"
 
 #ifdef _UNICODE
 #if defined _M_IX86
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#elif defined _M_IA64
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #elif defined _M_X64
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #else
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 #endif
-
-#include "Localization.h"
